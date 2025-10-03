6316391bfd803c9370716fd734702d3a479cb8cf
kimhieuwork <ngkimhieu14@gmail.com>
2025-10-03 12:53:33 +0700

    first commit

---
+++ PdfReader/MainForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace PdfReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImport = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pdfViewer1 = new PdfiumViewer.PdfViewer();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnImport
+            // 
+            this.btnImport.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnImport.Location = new System.Drawing.Point(0, 0);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(406, 54);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "Import";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.BtnImport_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnClear.Location = new System.Drawing.Point(402, 0);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(406, 54);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.BtnClear_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.btnImport);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(808, 54);
+            this.panel1.TabIndex = 2;
+            // 
+            // pdfViewer1
+            // 
+            this.pdfViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pdfViewer1.Location = new System.Drawing.Point(0, 54);
+            this.pdfViewer1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pdfViewer1.Name = "pdfViewer1";
+            this.pdfViewer1.Size = new System.Drawing.Size(808, 470);
+            this.pdfViewer1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 524);
+            this.Controls.Add(this.pdfViewer1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "PDF Reader";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Panel panel1;
+        private PdfiumViewer.PdfViewer pdfViewer1;
+    }
+}
+
---
+++ PdfReader/MainForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using PdfiumViewer;
+
+namespace PdfReader
+{
+    public partial class Form1 : Form
+    {
+        private PdfViewer pdfViewer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            //InitUI();
+        }
+
+        private void InitUI()
+        {
+            // Tạo control PdfViewer
+            pdfViewer = new PdfViewer
+            {
+                Dock = DockStyle.Fill,
+                ZoomMode = PdfViewerZoomMode.FitBest
+            };
+
+            // Tạo nút Import
+            Button btnImport = new Button
+            {
+                Text = "Import",
+                Dock = DockStyle.Top,
+                Height = 40
+            };
+            btnImport.Click += BtnImport_Click;
+
+            // Tạo nút Clear
+            Button btnClear = new Button
+            {
+                Text = "Clear",
+                Dock = DockStyle.Top,
+                Height = 40
+            };
+            btnClear.Click += BtnClear_Click;
+
+            // Thêm control vào form
+            this.Controls.Add(pdfViewer);
+            this.Controls.Add(btnClear);
+            this.Controls.Add(btnImport);
+        }
+
+        private void BtnImport_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "PDF files (*.pdf)|*.pdf";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        var doc = PdfDocument.Load(ofd.FileName);
+                        pdfViewer1.Document = doc;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Lỗi đọc file: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            pdfViewer1.Document = null;
+        }
+    }
+}
